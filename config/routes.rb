1cd89b91edb2c14bc544c3de44b62421a1169b4a
ElijahBcodes <eli1inthesnow@gmail.com>
2022-09-14 21:01:23 -0700

    index, create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :sightings
   resources :animals
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # get 'sightings' => 'sighting#index', as: 'exercises'
+  # post 'sightings' => 'sighting#create'
+  # get 'sightings' => 'sighting#new' as: 'new_sighting'
   # Defines the root path route ("/")
   # root "articles#index"
 end
